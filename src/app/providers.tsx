2366ff5a6db0b247c85cc0f3f275aa1c9e514073
Yousif9o5 <Yousuf1212121@gmail.com>
2024-01-12 17:56:12 +0300

    update providers add axios defaults

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,7 +1,17 @@
 "use client";
 
 import { NextUIProvider } from "@nextui-org/react";
+import { apis } from "api/apis";
+import axios from "axios";
 import { RecoilRoot } from "recoil";
+
+/**
+ * @axios defaults
+ */
+axios.defaults.baseURL = apis.baseUrlV1;
+axios.defaults.headers.common.Authorization = `Bearer ${
+  localStorage.getItem("token") ?? ""
+}`;
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
