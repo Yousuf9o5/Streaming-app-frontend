0620a8d6eab9bdc23e9a36085d8827ca3e369551
yousif <yousuf1212121@gmail.com>
2024-02-06 23:23:06 +0300

    update providers

--- src/utils/providers.tsx
+++ src/utils/providers.tsx
@@ -1,18 +1,22 @@
 "use client";
 
 import { NextUIProvider } from "@nextui-org/react";
-import { Apis } from "api/index";
+import URLS from "api/URLS";
 import axios from "axios";
 import Container from "layouts/Container";
 import { RecoilRoot } from "recoil";
 import Theme from "./theme";
 import Cookies from "js-cookie";
 
-/**
- * @axios defaults
- */
-axios.defaults.baseURL = Apis.baseUrlV1;
-axios.defaults.headers.common.Authorization = "Bearer " + Cookies.get("token");
+let token = "";
+
+if (typeof window != "undefined") {
+  token = localStorage.getItem("token") as string;
+}
+
+//defaults
+axios.defaults.baseURL = URLS.baseUrlV1;
+axios.defaults.headers.common.Authorization = "Bearer " + token;
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
