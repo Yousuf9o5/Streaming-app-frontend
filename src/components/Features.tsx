cad59b142cc4d10f8053586295f9f90692f8481d
yousif <yousuf1212121@gmail.com>
2024-02-06 23:21:11 +0300

    update features component

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -6,8 +6,11 @@ import { cardsData } from "../data/featuresData";
 function Features() {
   return (
     <Container className="w-full !grid grid-cols-3 gap-1 py-6 px-3">
-      {cardsData.map((item) => (
-        <div className="flex flex-col items-center gap-2 rounded-lg h-[230px] p-5 vertical-slide hover:shadow-small transition-shadow">
+      {cardsData.map((item, i) => (
+        <div
+          className="flex flex-col items-center gap-2 rounded-lg h-[230px] p-5 vertical-slide hover:shadow-small transition-shadow"
+          key={String(i) + "-cardsData"}
+        >
           {/* icon */}
           <div className="w-[60px] h-[60px]">
             <Image src={item.icon} alt="" className="w-full h-full" />
