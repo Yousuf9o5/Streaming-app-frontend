51005bfbf0b84c1e5bfcd4bc43d03cff14c9dd45
yousif <yousuf1212121@gmail.com>
2024-02-06 23:20:45 +0300

    rename index api to URLS

---
+++ src/api/URLS.ts
@@ -0,0 +1,22 @@
+export default class URLS {
+  // static readonly baseUrlV1 = "http://192.168.200.19:3000/api/v1";
+
+  static readonly root = "http://localhost:3001";
+
+  static readonly baseUrlV1 = "http://localhost:3001/api/v1";
+
+  static readonly home = "";
+
+  static readonly login = "user/login";
+
+  static readonly signup = "user/signup";
+
+  static readonly GetUserMovies = "user/get/movies";
+
+  static readonly UserInformation = "user/get/my-information";
+
+  // room apis
+  static readonly CreateRoom = "movie/create";
+
+  // movie id
+}
