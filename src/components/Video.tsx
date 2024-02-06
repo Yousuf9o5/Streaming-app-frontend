aca56c1cc0b50a6f830681b9972fb61c0b9aacac
yousif <yousuf1212121@gmail.com>
2024-02-06 23:21:57 +0300

    modify video component

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -12,28 +12,24 @@ function Video({ videoInf, videoUrl }: Props) {
   const { query } = useRouter();
   const [data, setData] = useState({});
 
-  useEffect(() => {
-    const config: AxiosRequestConfig = {
-      method: "get",
-      url: "/movies/" + query.id,
-    };
-    axios(config)
-      .then((res) => {
-        console.log(res);
-      })
-      .catch((err) => {
-        console.log("err");
-      });
-  }, []);
+  useEffect(() => {}, []);
 
   return (
-    <div className="min-w-[70dvw] w-full px-4">
+    <div className="min-w-[70dvw] w-full px-4 flex flex-col gap-4">
       <video width="320" height="240" controls className="w-full rounded-xl">
         <source
           src="https://www.youtube.com/watch?v=zWh3CShX_do"
           type="video/mp4"
         />
       </video>
+
+      <h2 className="text-h2">Lorem, ipsum dolor sit amet ?</h2>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. A animi
+        quisquam, autem et, debitis minus alias dolorem officia quidem omnis
+        similique dignissimos rerum facere molestias unde quasi eos, blanditiis
+        esse.
+      </p>
     </div>
   );
 }
