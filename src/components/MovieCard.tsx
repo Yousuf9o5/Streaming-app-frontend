192fee146c24269d9882d585e75153809608729c
yousif <yousuf1212121@gmail.com>
2024-02-09 21:11:30 +0300

    update movie card

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,6 +1,6 @@
 import { CreateRoom } from "api/room";
 import Link from "next/link";
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Nodes } from "types/react.types";
 import MovieCardLink from "./MovieCardLink";
 import { Video } from "types/elements.types";
@@ -21,34 +21,56 @@ function MovieCard(props: Props) {
   const [seek, setSeek] = useState(0);
   const [playing, setPlaying] = useState(false);
 
+  useEffect(() => {
+    if (!playing) return;
+    if (!seek) {
+      setSeek((prev) => prev + 10);
+      return;
+    }
+    const { current } = videoRef;
+    const videoDuration = current?.getDuration()!;
+
+    const interval = setInterval(
+      () => setSeek((prev) => (prev + 10) % videoDuration),
+      1500
+    );
+
+    current?.seekTo(seek, "seconds");
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [seek, playing]);
+
   const onMouseEnter = () => {
-    // const { current } = videoRef;
-
-    setPlaying(!playing);
-    // let time = current?.play();
-    // console.log(time);
-
-    // current?.seekTo(5000, "seconds");
-    console.log(playing);
+    setPlaying(true);
+  };
+  const onMouseLeave = () => {
+    setPlaying(false);
+    videoRef.current?.seekTo(0, "seconds");
+    setSeek(0);
   };
 
   return (
     <article
       onMouseEnter={onMouseEnter}
-      className="rounded-xl bg-white p-3 shadow-lg hover:shadow-xl hover:transform hover:scale-105 duration-300 bg-secondary-theme"
+      onMouseLeave={onMouseLeave}
+      className="rounded-2xl bg-secondary-theme shadow-lg hover:shadow-xl hover:transform hover:scale-105 duration-300 overflow-hidden"
     >
-      <MovieCardLink>
-        <div className="mt-1 p-2">
+      <MovieCardLink movieId={movieId} roomId={roomId}>
+        <div>
           <ReactPlayer
             ref={videoRef}
             width="100%"
             height="50%"
             className="rounded"
             url={url}
-            playing
+            playing={playing}
             loop
           />
-          <h2 className="text-slate-700">{name}</h2>
+        </div>
+        <div className="p-3 text-left">
+          <h2 className="text-slate-700 font-bold text-large">{name}</h2>
           <p className="mt-1 text-sm text-slate-400">{desc}</p>
         </div>
       </MovieCardLink>
