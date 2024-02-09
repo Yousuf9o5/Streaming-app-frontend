eb4d6ae7f63ea2dfa3b4e4df68ec6494a3f1f99c
yousif <yousuf1212121@gmail.com>
2024-02-09 21:10:26 +0300

    update urls

--- src/api/URLS.ts
+++ src/api/URLS.ts
@@ -1,9 +1,9 @@
 export default class URLS {
   // static readonly baseUrlV1 = "http://192.168.200.19:3000/api/v1";
 
-  static readonly root = "http://localhost:3001";
+  static readonly root = "http://192.168.0.112:3001";
 
-  static readonly baseUrlV1 = "http://localhost:3001/api/v1";
+  static readonly baseUrlV1 = "http://192.168.0.112:3001/api/v1";
 
   static readonly home = "";
 
@@ -16,7 +16,10 @@ export default class URLS {
   static readonly UserInformation = "user/get/my-information";
 
   // room apis
-  static readonly CreateRoom = "movie/create";
+  static readonly CreateRoom = "room/create";
+  static readonly GetRoomById = "room/get/by-id/";
+  static readonly GetRoomMembers = "room/get/members";
 
   // movie id
+  static readonly GetMovieById = "movie/get/by-id/";
 }
