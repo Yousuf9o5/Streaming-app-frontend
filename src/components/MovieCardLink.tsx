10efcc8ad7cd4cc1322ea9d0575707b0065eec6b
yousif <yousuf1212121@gmail.com>
2024-02-09 21:11:41 +0300

    update movie card link

--- src/components/MovieCardLink.tsx
+++ src/components/MovieCardLink.tsx
@@ -1,21 +1,23 @@
 import { CreateRoom } from "api/room";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { Nodes } from "types/react.types";
 
 type Props = {
-  roomId?: string;
+  roomId?: string | null;
   movieId?: string;
   children: Nodes;
 };
 
 function MovieCardLink(props: Props) {
   const { children, movieId, roomId } = props;
+  const router = useRouter();
 
   /** @Yousuf9o5 create room function */
   const onClick = () => {
     CreateRoom(movieId!)
       .then((res) => {
-        console.log(res.msg);
+        router.push("/room/" + res.data.id);
       })
       .catch(() => console.log("Error Creating Room"))
       .finally(() => {});
@@ -23,14 +25,20 @@ function MovieCardLink(props: Props) {
 
   if (!roomId) {
     return (
-      <button className="bg-secondary-theme w-full h-full" onClick={onClick}>
+      <button
+        className="bg-secondary-theme w-full h-full p-0"
+        onClick={onClick}
+      >
         {children}
       </button>
     );
   }
 
   return (
-    <Link href={`/room/${roomId}`} className="w-full h-full">
+    <Link
+      href={`/room/${roomId}`}
+      className="block bg-secondary-theme w-full h-full !p-0"
+    >
       {children}
     </Link>
   );
