43dffe96af6c21c3876c0efc00390afc474891e6
yousif <yousuf1212121@gmail.com>
2024-02-09 21:12:10 +0300

    update app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,16 @@ import "style/main.css";
 import { Providers } from "utils/providers";
 import { AppProps } from "next/app";
 import Authentication from "utils/authentication";
+import { useEffect } from "react";
+import socket from "../socket";
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    socket.on("connect", () => {
+      console.log("connected");
+    });
+  }, []);
+
   return (
     <Providers>
       {/* <Head></Head> */}
