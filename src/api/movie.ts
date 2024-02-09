4f07f8369d8f7a2616f8d46b8177964614d72886
yousif <yousuf1212121@gmail.com>
2024-02-09 21:10:05 +0300

    update movie file

--- src/api/movie.ts
+++ src/api/movie.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosRequestConfig } from "axios";
 import URLS from "./URLS";
-import { ArrRes, MovieAttributes } from "types/response.types";
+import { ArrRes, MovieAttributes, ObjRes } from "types/response.types";
 
 export async function GetUserMovies() {
   const config: AxiosRequestConfig = {
@@ -9,6 +9,17 @@ export async function GetUserMovies() {
   };
 
   const callback: ArrRes<MovieAttributes> = (res) => res.data;
+
+  return axios(config).then(callback);
+}
+
+export async function GetMoviesById(id: string) {
+  const config: AxiosRequestConfig = {
+    method: "GET",
+    url: URLS.GetMovieById + id,
+  };
+
+  const callback: ObjRes<MovieAttributes> = (res) => res.data;
 
   return axios(config).then(callback);
 }
